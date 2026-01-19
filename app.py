63ea1e421143f02e63cfa6b7abd75599aaceb189
aldre-arch <keyzar2002@gmail.com>
2026-01-19 08:21:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import urllib.parse
 st.set_page_config(page_title="Product Recommendation Library", layout="wide")
 
 # --- GITHUB RAW URL CONFIGURATION ---
-GITHUB_RAW_BASE = "https://raw.githubusercontent.com/aldre-arch/TN-Product-Reccomendation/main/"
+GITHUB_RAW_BASE = "https://raw.githubusercontent.com/aldre-arch/TN-Product-Reccomendation/main/static"
 
 # --- CUSTOM CSS ---
 st.markdown("""
